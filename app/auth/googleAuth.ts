44a18a41551a2bda659490cb54c33ee86f24802a
Julia Tan <juliatan@users.noreply.github.com>
2024-12-19 22:44:32 +0000

    🔐 Use encoded JWT

--- app/auth/googleAuth.ts
+++ app/auth/googleAuth.ts
@@ -4,13 +4,20 @@ import { GoogleAuth } from 'google-auth-library';
 let googleAuthInstance: GoogleAuth | null = null;
 
 const createGoogleAuth = () => {
+  const privateKey = Buffer.from(
+    process.env.GOOGLE_JWT_PRIVATE_KEY || '',
+    'base64'
+  )
+    .toString('utf8')
+    .replace(/\\n/g, '\n');
+
   if (!googleAuthInstance) {
     googleAuthInstance = new auth.GoogleAuth({
       credentials: {
         type: 'service_account',
         project_id: process.env.GOOGLE_JWT_PROJECT_ID,
         private_key_id: process.env.GOOGLE_JWT_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_JWT_PRIVATE_KEY,
+        private_key: privateKey,
         client_email: process.env.GOOGLE_JWT_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_JWT_CLIENT_ID,
         universe_domain: 'googleapis.com',
