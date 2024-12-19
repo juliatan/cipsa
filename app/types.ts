2b9269c198d4b0bf77d6da3eb3dfada6e76f67c9
Julia Tan <juliatan@users.noreply.github.com>
2024-12-19 19:00:25 +0000

    🏷️ Add types.ts

---
+++ app/types.ts
@@ -0,0 +1,31 @@
+export type OptionsType = string[];
+export type FieldName =
+  | 'name'
+  | 'password'
+  | 'qualifyingFirst'
+  | 'qualifyingSecond'
+  | 'qualifyingThird'
+  | 'raceFirst'
+  | 'raceSecond'
+  | 'raceThird';
+
+export type QualifyingOrRaceField = { id: FieldName; label: string };
+
+export type PlayerName = 'Johann' | 'Momo' | 'Oli' | 'Stefania';
+
+export type SubmitFormData = {
+  name: PlayerName;
+  password: string;
+  qualifyingFirst: string;
+  qualifyingSecond: string;
+  qualifyingThird: string;
+  raceFirst: string;
+  raceSecond: string;
+  raceThird: string;
+}
+
+export type SubmitFormState = {
+  message?: string;
+  error?: string;
+  formData: SubmitFormData;
+};
